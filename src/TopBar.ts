2bb6a76e62228166485862fdfd7342f63a6aacb8
cjsellwood <c.sellwood22@gmail.com>
2021-11-02 11:09:57 +1100

    Made play button start new game or restart game if currently running

---
+++ src/TopBar.ts
@@ -0,0 +1,84 @@
+export class TopBar {
+  private score: number;
+  private scoreElement: HTMLSpanElement;
+  private highScore: number;
+  private highScoreElement: HTMLSpanElement;
+  private playButton: HTMLButtonElement;
+  public running: Boolean;
+  public restarting: Boolean;
+  private runGame: Function;
+
+  constructor(runGame: Function) {
+    this.score = 0;
+    this.scoreElement = document.getElementById("score")! as HTMLSpanElement;
+    this.highScore = 0;
+    this.highScoreElement = document.getElementById(
+      "high-score"
+    )! as HTMLSpanElement;
+    this.playButton = document.getElementById(
+      "play-button"
+    )! as HTMLButtonElement;
+    this.running = false;
+    this.restarting = false;
+    this.runGame = runGame;
+
+    this.initialize();
+  }
+
+  // Add one to score and store if new high score
+  incrementScore() {
+    this.score += 1;
+    this.scoreElement.textContent = this.score.toString();
+
+    // Update high score if less than score
+    if (this.score > this.highScore) {
+      this.highScore = this.score;
+      this.highScoreElement.textContent = this.highScore.toString();
+      localStorage.setItem("highScore", this.highScore.toString());
+    }
+  }
+
+  // End game on lose condition
+  endGame() {
+    this.running = false;
+    this.playButton.textContent = "Play";
+  }
+
+  // Clear interval of past game and start a new game
+  restartGame(interval: NodeJS.Timer) {
+    this.restarting = false;
+    this.score = 0;
+    this.scoreElement.textContent = this.score.toString();
+    clearInterval(interval);
+    this.startGame();
+  }
+
+  // Run the game loop
+  startGame() {
+    this.runGame();
+  }
+
+  private initialize() {
+    // Add event listener for play/restart button
+    this.playButton.addEventListener("click", () => {
+      // If not running start a new game
+      if (!this.running) {
+        this.restarting = false;
+        this.running = true;
+        this.score = 0;
+        this.scoreElement.textContent = this.score.toString();
+        this.playButton.textContent = "Restart";
+        this.startGame();
+      } else {
+        // If already running exit current game and start a new game
+        this.restarting = true;
+      }
+    });
+
+    // Retrieve previous high score
+    const storedScore = localStorage.getItem("highScore");
+    if (storedScore) {
+      this.highScoreElement.textContent = storedScore;
+    }
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,7 @@ import "./app.css";
 import { Board } from "./Board";
 import { Snake } from "./Snake";
 import { Fruit } from "./Fruit";
-
-const height = 24;
-const width = 24;
-const board = new Board(
-  height,
-  width,
-  document.getElementById("root")! as HTMLDivElement
-);
+import { TopBar } from "./TopBar";
 
 const runGame = () => {
   const snake = new Snake(height);
@@ -19,12 +12,19 @@ const runGame = () => {
   board.renderFruit(fruit.position);
 
   const gameLoop = setInterval(() => {
+    // Press restart button will stop function
+    if (topBar.restarting) {
+      topBar.restartGame(gameLoop);
+      return;
+    }
+
     // Move snake and check if fruit collected
     const fruitCollected = snake.move(fruit.position);
 
     if (fruitCollected) {
       fruit.collect(snake.parts);
       board.renderFruit(fruit.position);
+      topBar.incrementScore();
     }
     board.renderSnake(snake.parts);
 
@@ -36,6 +36,17 @@ const runGame = () => {
     if (hitEdge || hitSelf) {
       clearInterval(gameLoop);
       console.log(fruit.collected);
+      topBar.endGame();
     }
   }, 100);
 };
+
+const height = 24;
+const width = 24;
+const board = new Board(
+  height,
+  width,
+  document.getElementById("root")! as HTMLDivElement
+);
+
+const topBar = new TopBar(runGame);
