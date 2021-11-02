4606e613a05e9b59260b864bb07aaa43c82c681d
cjsellwood <c.sellwood22@gmail.com>
2021-11-02 12:05:29 +1100

    Added favicons

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   entry: "./src/app.ts",
@@ -25,6 +26,26 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/icon.png",
+      // inject: true
+      favicons: {
+        icons: {
+          android: [
+            "android-chrome-192x192.png",
+            "android-chrome-144x144.png",
+            "android-chrome-512x512.png",
+          ],
+          appleIcon: ["apple-touch-icon-180x180.png"],
+          appleStartup: false,
+          coast: false,
+          favicons: ["favicon-16x16.png", "favicon-32x32.png"],
+          firefox: false,
+          windows: false,
+          yandex: false,
+        },
+      },
     }),
   ],
   resolve: {
